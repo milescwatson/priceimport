8b6c7425c91e26253959e6f7fc0da3540a41c8a6
Miles Watson <mc.watson415@gmail.com>
2025-08-16 16:25:32 -0700

    Try process.env.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,8 @@
-import { INTUIT_CLIENT_ID, INTUIT_REDIRECT_URI, INTUIT_SCOPE } from '$env/dynamic/private';
+// import { INTUIT_CLIENT_ID, INTUIT_REDIRECT_URI, INTUIT_SCOPE } from '$env/dynamic/private';
 // https://appcenter.intuit.com/app/connect/oauth2/authorize?client_id=ABZbmhpDTZaQfqp5ILnYLCIoHu2hEEhDmyfpX75VjexlVc0pMg&scope=com.intuit.quickbooks.accounting&redirect_uri=https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl&response_type=code&state=PlaygroundAuth
 /** @type {import('./$types').PageLoad} */
 export function load() {
+    const { INTUIT_CLIENT_ID, INTUIT_REDIRECT_URI, INTUIT_SCOPE } = process.env;
     return {
         authorizationUrl: `https://appcenter.intuit.com/app/connect/oauth2/authorize?client_id=${INTUIT_CLIENT_ID}&scope=${INTUIT_SCOPE}&redirect_uri=${INTUIT_REDIRECT_URI}&response_type=code&state=expected_state`
     };
